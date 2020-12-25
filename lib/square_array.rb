72622856b4cc2b09a96d6d64da08b59a955a5fd2
C. L <amautacelt@gmail.com>
2020-12-25 07:01:30 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -10,4 +10,16 @@ def square_array(numbers)
   new_numbers
 end
 
-square_array([1,2,3])
+#square_array([1,2,3])
+
+
+def square_array(numbers)
+  counter = 0
+  sqr_numbers = []
+  while numbers[counter] do
+    sqr_numbers<<numbers[counter]*numbers[counter]
+    counter += 1
+  end
+  sqr_numbers
+end
+
